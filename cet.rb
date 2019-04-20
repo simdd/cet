4513777b7ae512dc304f14319f0da750866a6d06
a <dev.simdd@gmail.com>
2019-04-20 23:07:52 +0800

    update

--- cet.rb
+++ cet.rb
@@ -5,7 +5,7 @@ class Cet < Formula
     sha256 "66525b2f057c4247d721b7a20464bc3dca641203cad0da751d15b19953092795"
 
     def install
-      prefix.install "cetrc.sh"
+      buildpath.install Dir["~/.cetrc"]
       bin.install "cet"
    end
   
