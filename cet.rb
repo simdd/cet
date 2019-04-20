1c29f88311dda2547b5a3e4ce9eb6e0162655027
a <dev.simdd@gmail.com>
2019-04-20 13:07:40 +0800

    fix: download version

--- cet.rb
+++ cet.rb
@@ -1,7 +1,7 @@
 class Cet < Formula
     desc "cet"
     homepage "https://github.com/simdd/cet"
-    url "https://github.com/simdd/cet/releases/download/0.0.2/cet.zip"
+    url "https://github.com/simdd/cet/releases/download/0.0.3/cet.zip"
     sha256 "234f4b309e311092dafe81fd48782a6f88904242f663fc5c2ab98abc0e721137"
 
     def install
